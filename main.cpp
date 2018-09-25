6e34b78bc59f04406316e11e77df4d151c7378f5
Niedziol <michalekniedziolka@gmail.com>
2018-09-25 22:05:50 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,43 @@
 #include <cstdio>
 #include <allegro5/allegro.h>
 
-void must_init(bool test, const char *desc){
+void check_init(bool test, const char *desc){
     if (test) return;
     
-    printf("couldn't initialize %s\n", desc);
+    fprintf(stderr, "couldn't initialize %s\n", desc);
     exit(-1);
 }
 
 int main(int argc, char **argv){
 
     //initialize allegro
-    must_init(al_init(), "allegro");
+    check_init(al_init(), "allegro");
 
     //install keyboard
-    must_init(al_install_keyboard(), "keyboard");
+    check_init(al_install_keyboard(), "keyboard");
 
     ALLEGRO_KEYBOARD_STATE keyboard;
 
     //create display
     ALLEGRO_DISPLAY* display = nullptr;
 
-    display = al_create_display(1200, 900);
+    display = al_create_display(960, 540);
     al_set_window_title(display, "Check Window");
-    must_init(display, "display")
+    check_init(display, "display");
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
-    must_init(timer, "timer");
+    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
+    check_init(timer, "timer");
 
+    ALLEGRO_BITMAP *spaceship = al_load_bitmap("pic/spaceship.png");
+
+    check_init(spaceship, "spaceship");
     al_clear_to_color(al_map_rgb(0,0,0));
-
+    al_draw_bitmap (spaceship,0,0,0);
     al_flip_display();
 
     al_rest(5.0);
 
     al_destroy_display(display);
-
+    al_destroy_bitmap(spaceship);
     return 0;
 }
