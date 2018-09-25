3ff537e21bc41e759f23d767302932a29d6ba447
Hodrin <szczesciak4@o2.pl>
2018-09-25 21:38:26 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <cstdio>
 #include <allegro5/allegro.h>
 
+void must_init(bool test, const char *desc){
+    if (test) return;
+    
+    printf("couldn't initialize %s\n", desc);
+    exit(-1);
+}
+
 int main(int argc, char **argv){
 
     //initialize allegro
-    if(!al_init()) {
-        fprintf(stderr, "failed to initialize allegro!\n");
-        return -1;
-    }
+    must_init(al_init(), "allegro");
 
     //install keyboard
-    if(!al_install_keyboard()){
-        fprintf(stderr, "failed to install keyboard\n");
-        return -1;
-    }
+    must_init(al_install_keyboard(), "keyboard");
+
     ALLEGRO_KEYBOARD_STATE keyboard;
 
     //create display
-    ALLEGRO_DISPLAY *display = nullptr;
+    ALLEGRO_DISPLAY* display = nullptr;
 
     display = al_create_display(1200, 900);
-    al_set_window_title( display,"Check Window");
-    if(!display) {
-        fprintf(stderr, "failed to create display!\n");
-        return -1;
-    }
+    al_set_window_title(display, "Check Window");
+    must_init(display, "display")
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    must_init(timer, "timer");
 
     al_clear_to_color(al_map_rgb(0,0,0));
 
