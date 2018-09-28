2dd7404f08f644898a15cc2e219725e86b51326d
Niedziol <michalekniedziolka@gmail.com>
2018-09-28 19:35:14 +0200

    upload check for collision

--- object.cpp
+++ object.cpp
@@ -15,5 +15,24 @@ void Object::move(Point vec){
 }
 
 bool Object::check_collision(Object obstacle){
+    for(int k = 0; k < this -> n; k++){
+        int x = this -> vertices[k].x, y = this -> vertices[k].y;
+        int i, j = 0;
+        bool oddNODES = false;
+        for (i = 0; i < obstacle.n; i++){
+            j++;
+            if (j == obstacle.n){
+                j = 0;
+            }
+            if (obstacle.vertices[i].y < y && obstacle.vertices[j].y >= y || obstacle.vertices[j].y < y && obstacle.vertices[i].y >= y){
+                if (obstacle.vertices[i].x + (y - obstacle.vertices[i].y) / (obstacle.vertices[j].y - obstacle.vertices[i].y) * (obstacle.vertices[j].x - obstacle.vertices[i].x) < x){
+                    oddNODES = !oddNODES;
+                }
+            }
+        }
+        if(oddNODES)
+            return oddNODES;
+    }
+    return false;
     
 }
